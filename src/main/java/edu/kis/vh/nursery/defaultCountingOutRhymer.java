f8afa3d3c4ce4a0ac2112fc52d186846cbe5e06c
Dmytro Malinovskyi <252501@edu.p.lodz.pl>
2025-03-13 03:16:41 +0100

    4.3 bledy konwencji nazewniczej pol klas

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,34 +1,40 @@
 package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
+	
+	private static final int CAPACITY = 12;
+    private static final int EMPTY_INDEX = -1;
+    private static final int FULL_INDEX = 11;
 
-    private int[] NUMBERS = new int[12];
-
-    public int total = -1;
+    private int[] numbers = new int[CAPACITY];
+    private int total = EMPTY_INDEX;
 
     public void countIn(int in) {
-        if (!isFull())
-            NUMBERS[++total] = in;
+        if (!isFull()) {
+            numbers[++total] = in;
+        }
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_INDEX;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_INDEX;
     }
 
     protected int peekaboo() {
-        if (callCheck())
-            return -1;
-        return NUMBERS[total];
+        if (callCheck()) {
+            return EMPTY_INDEX;
+        }
+        return numbers[total];
     }
 
     public int countOut() {
-        if (callCheck())
-            return -1;
-        return NUMBERS[total--];
+        if (callCheck()) {
+            return EMPTY_INDEX;
+        }
+        return numbers[total--];
     }
 
 }
