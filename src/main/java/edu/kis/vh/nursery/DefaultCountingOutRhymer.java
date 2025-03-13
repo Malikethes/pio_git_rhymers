0b4bba6781acbec840c549a1df66b8fbcbcbbe51
Dmytro Malinovskyi <252501@edu.p.lodz.pl>
2025-03-13 03:07:06 +0100

    4.1 poprawa nazw klas
    # Conflicts:
    #	src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,14 @@
 package edu.kis.vh.nursery;
 
+<<<<<<< HEAD:src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
 public class defaultCountingOutRhymer {
 	
 	private static final int CAPACITY = 12;
     private static final int EMPTY_INDEX = -1;
     private static final int FULL_INDEX = 11;
+=======
+public class DefaultCountingOutRhymer {
+>>>>>>> 0ec0a8d (4.1 poprawa nazw klas):src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
 
     private int[] numbers = new int[CAPACITY];
     private int total = EMPTY_INDEX;
