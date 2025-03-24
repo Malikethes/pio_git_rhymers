c51d34cf70d438002e1efe8d3d4d4f7cf62abae9
Dmytro Malinovskyi <252501@edu.p.lodz.pl>
2025-03-24 21:42:37 +0100

    17.Wygenerowano i uzupełniono szkielet dokumentacji dla klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,23 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa reprezentująca implementację systemu "FIFO" (First In, First Out) dla licznika.
+ * Dziedziczy po klasie DefaultCountingOutRhymer i implementuje metodę countOut(),
+ * która umożliwia liczenie w porządku FIFO.
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
+	/**
+     * Tymczasowy obiekt DefaultCountingOutRhymer wykorzystywany do przechowywania elementów
+     * podczas operacji na danych w metodzie countOut().
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Wyjmuje liczbę z obiektu FIFORhymer zgodnie z zasadą FIFO. 
+     * Metoda ta tymczasowo przechowuje elementy w pomocniczym obiekcie `temp`, 
+     * aby wyciągnąć najstarszy element z oryginalnej struktury.
+     */
     @Override
     public int countOut() {
         while (!callCheck())
